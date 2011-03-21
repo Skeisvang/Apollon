4d92cc7fe3f837d8bfbc3e216f0ee637fe7e4cce
audun hauge <au@phobos.skeisvang>
2011-03-21 10:53:45 +0100

    første versjon med index og database

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+// En kort innføring i GIT
+//
+// rediger en fil (gjerne fra dreamweaver)
+//   dersom dette er en ny fil:
+//   skriv
+//     git add navn_paa_fil
+//
+//   Dersom du bare har redigert   
+//   skriv 
+//     git commit -a -m "audun: La til ett nytt menyvalg"
+//
+// Dersom du ønsker å jobbe på en lokal kopi av prosjektet skal du
+// gjøre følgende på din konto:
+//   cd public_html
+//   git clone 152.93.79.18:/home/apollon/public_html/artikler
+//   
+//
+//
+// Dette bør være startsida for prosjektet.
+// Legg inn meny osv
+// Dere får vurdere om dere vil bruke templates
+// Jeg har en følelse av at det lett kan skjære seg dersom flere
+// brukere skal redigere sider basert på templates ...
+
+
+print "Heisan";
+
+
+
+
+
+
+// siste linja er tom med vilje
+// php har en tendens til å bli sur dersom
+// du har tomme linjer etter siste  ?  >
+// Derfor slutter denne filen uten denne taggen
+// Det fungerer mye bedre
+
