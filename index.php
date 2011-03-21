94d3fdb3d3a56b5ce7536126b92389420ca68898
apollon <apollon@phobos.skeisvang>
2011-03-21 11:54:02 +0100

    redigerte innledningen

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
-// 21 mars 2011
-// En kort innføring i GIT
-//
-// rediger en fil (gjerne fra dreamweaver)
-//   dersom dette er en ny fil:
-//   skriv
-//     git add navn_paa_fil
-//
-//   Dersom du bare har redigert   
-//   skriv 
-//     git commit -a -m "audun: La til ett nytt menyvalg"
-//
-//       Dersom du ønsker å jobbe på en lokal kopi av prosjektet skal du
-// gjøre følgende på din konto:
-//   cd public_html
-//   git clone 152.93.79.18:/home/apollon/public_html/artikler
-//   
-//
+//  versjon 1 - 21 mars 2011
 //
 // Dette bør være startsida for prosjektet.
 // Legg inn meny osv
