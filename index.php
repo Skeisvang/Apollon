59e15545aad061abf36d5ed9d8fbeaa3401c4644
Henry <henryss@me.com>
2011-03-23 12:20:19 +0100

    Ryddet opp i kode. Fjernet php i Index. La til divene menu og footer.

--- index.php
+++ index.php
@@ -1,24 +1,18 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-	<head>
+<head>
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+</head>
+<body>
+<div id="menu">
 
-<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
-	</head>
-	<body>
+</div>
 <div id="content">
-<?php
-//  versjon 1 - 21 mars 2011
-//
-// Dette bør være startsida for prosjektet.
-// Legg inn meny osv
-// Dere får vurdere om dere vil bruke templates
-// Jeg har en følelse av at det lett kan skjære seg dersom flere
-// brukere skal redigere sider basert på templates ...
 
+</div>
+<div id="footer">
 
-print "Heisan";
-?>
 </div>
 	</body>
 </html>
