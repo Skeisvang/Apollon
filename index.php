c2a611a81136a084a10b5600639fb0c11b9a6abb
Henry <henryss@me.com>
2011-03-23 11:08:04 +0100

    Ordnet på index filen. Muligens bare tull. Sikrer kompatibilitet med IE. lagt til div id=content.

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+	</head>
+	<body>
+<div id="content">
 <?php
 //  versjon 1 - 21 mars 2011
 //
@@ -9,9 +18,7 @@
 
 
 print "Heisan";
-
-
-
-
-
 ?>
+</div>
+	</body>
+</html>
