d56b24552363be53d32a897d2b11b5df3bdeff94
Henry <henryss@me.com>
2011-03-23 11:53:04 +0100

    La til css fil

--- brukere.php
+++ brukere.php
@@ -56,7 +56,8 @@ $totalPages_brukere = ceil($totalRows_brukere/$maxRows_brukere)-1;
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
+<link rel="stylesheet" href="page.css" type="text/css" />
+<title>Brukerliste</title>
 </head>
 
 <body>
