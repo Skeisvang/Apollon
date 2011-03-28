04cda8df6672363c6d86b88172c106e8819f2884
Henry <henryss@me.com>
2011-03-28 12:58:07 +0200

    oppdaterte navnet login2 til login i alle filene

--- lastoppart.php
+++ lastoppart.php
@@ -62,7 +62,7 @@ function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) {
   return $isValid; 
 }
 
-$MM_restrictGoTo = "login2.php";
+$MM_restrictGoTo = "login.php";
 if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
   $MM_qsChar = "?";
   $MM_referrer = $_SERVER['PHP_SELF'];
