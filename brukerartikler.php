4fba4c9e963b364b2ca61f62e960bb3b7c8306d7
Michael Eikeland <michael.eikeland@gmail.com>
2011-03-30 10:27:52 +0200

    Brukerartikler og artikkel

--- brukerartikler.php
+++ brukerartikler.php
@@ -1,4 +1,25 @@
-<!--Her må vi få opp en liste over alle artiklene en bruker har skrevet.-->
-
-<!--Åpner en kontakt til SQL serveren.-->
-<?php require_once('Connections/apollon.php'); ?>
+<?php
+require_once('Connections/apollon.php');
+mysql_select_db($database_apollon, $apollon);
+if (!isset($_GET['feide']))
+	die("Du m� spesifisere feide-iden");
+$feide = $_GET['feide'];
+$q = "SELECT * FROM artikkel WHERE bruker_feide = '{$feide}'";
+$r = mysql_query($q, $apollon) or die(mysql_error());
+$artikkel = mysql_fetch_assoc($r);
+?>
+<table border="1">
+  <h2><?php echo $feide; ?> sine artikler</h2>
+  <tr>
+    <th>Overskrift</th>
+    <th>Artikkel</th>
+    <th>Publisert</th>    
+  </tr>
+  <?php do { ?>
+    <tr>
+      <td><a href="artikkel.php?id=<?php echo $artikkel['id'];?>"><?php echo $artikkel['overskrift']; ?></a></td>
+      <td><?php echo $artikkel['artikkel']; ?></td>
+      <td><?php echo $artikkel['publisert']; ?></td>
+    </tr>
+    <?php } while ($artikkel = mysql_fetch_assoc($r)); ?>
+</table>
