9597ab41217beca694b2758b2b99d5c50a908cff
Michael Eikeland <michael.eikeland@gmail.com>
2011-03-30 10:42:58 +0200

    Lagt til styling på sidene brukerartikler og artikkel

--- artikkel.php
+++ artikkel.php
@@ -11,6 +11,27 @@ if (mysql_num_rows($r) == 0)
 
 $artikkel = mysql_fetch_assoc($r);
 ?>
-<h2><?php echo $artikkel['overskrift']; ?></h2>
-<small>Skrevet av <?php echo $artikkel['bruker_feide']; ?></small>
-<p><?php echo nl2br($artikkel['artikkel']); ?></p>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+        <meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+        <link rel="stylesheet" href="page.css" type="text/css" />
+        <link rel="stylesheet" href="menu.css" type="text/css" />
+    </head>
+    <body>
+        <div id="page_header">
+        	<h1>Apollon</h1>
+        </div>        
+        <ul id="menu">
+            <li><a href="index.php">Hjem</a></li>
+            <li><a href="#">Artikler</a></li>
+            <li><a href="#">Brukeroversikt</a></li>
+        </ul>
+        <div id="page_content">
+            <h2><?php echo $artikkel['overskrift']; ?></h2>
+            <small>Skrevet av <?php echo $artikkel['bruker_feide']; ?></small>
+            <p><?php echo nl2br($artikkel['artikkel']); ?></p>
+        </div>
+    </body>
+</html>
--- brukerartikler.php
+++ brukerartikler.php
@@ -2,24 +2,44 @@
 require_once('Connections/apollon.php');
 mysql_select_db($database_apollon, $apollon);
 if (!isset($_GET['feide']))
-	die("Du må spesifisere feide-iden");
+	die("Du mÃ¥ spesifisere feide-iden");
 $feide = $_GET['feide'];
 $q = "SELECT * FROM artikkel WHERE bruker_feide = '{$feide}'";
 $r = mysql_query($q, $apollon) or die(mysql_error());
 $artikkel = mysql_fetch_assoc($r);
 ?>
-<table border="1">
-  <h2><?php echo $feide; ?> sine artikler</h2>
-  <tr>
-    <th>Overskrift</th>
-    <th>Artikkel</th>
-    <th>Publisert</th>    
-  </tr>
-  <?php do { ?>
-    <tr>
-      <td><a href="artikkel.php?id=<?php echo $artikkel['id'];?>"><?php echo $artikkel['overskrift']; ?></a></td>
-      <td><?php echo $artikkel['artikkel']; ?></td>
-      <td><?php echo $artikkel['publisert']; ?></td>
-    </tr>
-    <?php } while ($artikkel = mysql_fetch_assoc($r)); ?>
-</table>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+        <meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+        <link rel="stylesheet" href="page.css" type="text/css" />
+        <link rel="stylesheet" href="menu.css" type="text/css" />
+    </head>
+    <body>
+        <div id="page_header">
+        	<h1>Apollon</h1>
+        </div>        
+        <ul id="menu">
+            <li><a href="index.php">Hjem</a></li>
+            <li><a href="#">Artikler</a></li>
+            <li><a href="#">Brukeroversikt</a></li>
+        </ul>
+        <div id="page_content">
+            <table border="1">
+                <h2><?php echo $feide; ?> sine artikler</h2>
+                <tr>
+                    <th>Overskrift</th>
+                    <th>Publisert</th>    
+                </tr>
+                <?php do { ?>
+                <tr>
+                    <td><a href="artikkel.php?id=<?php echo $artikkel['id'];?>"><?php echo $artikkel['overskrift']; ?></a></td>
+                    <td><?php echo $artikkel['publisert']; ?></td>
+                </tr>
+                <?php } while ($artikkel = mysql_fetch_assoc($r)); ?>
+            </table>
+        </div>
+    </body>
+</html>
+       
