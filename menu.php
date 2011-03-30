e497ebeedf89ddfa1557fa16765e0479a7a396fb
Henry <henryss@me.com>
2011-03-30 13:04:45 +0200

    Add menu

---
+++ menu.php
@@ -0,0 +1,9 @@
+<div id="page_header">
+	<h1 id="logo">Apollon</h1>
+</div>
+
+<ul id="menu">
+	<li><a href="index.php">Hjem</a></li>
+	<li><a href="lastoppart.php">Ny Artikkel</a></li>
+	<li><a href="brukere.php">Brukeroversikt</a></li>
+</ul>
