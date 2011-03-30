ecb0d9887c780d3fcce1585728b8a329a425ad1f
Audun Hauge <audun.hauge@gmail.com>
2011-03-30 15:08:11 +0200

    finner publiserte artikler og deler (nesten) ut

--- tildel.php
+++ tildel.php
@@ -9,19 +9,26 @@ while($artlist[] = mysql_fetch_assoc($rc));
 array_pop($artlist);
 
 // finn alle publiserte artikler
-$sql = "SELECT a.id,a.overskrift,a.bruker_feide,count(k.id) as ant FROM artikkel a left join karakter k on (a.id = k.artikkel_id) where a.publisert is not null group by a.id";
+$sql = "SELECT a.id,a.overskrift,a.bruker_feide,count(k.id) as ant FROM artikkel a left join karakter k on (a.id = k.artikkel_id) 
+	    where a.publisert is not null group by a.id  order by ant";
 $rc = mysql_query($sql, $apollon) or die(mysql_error());
 $published = array();
 while($published[] = mysql_fetch_assoc($rc));
 array_pop($published);
 
-// finn alle vurderte artikler
+// finn alle artikler som er delt ut til vurdering
 $sql = "SELECT k.artikkel_id, count(id) as ant FROM karakter k group by k.artikkel_id ";
 $rc = mysql_query($sql, $apollon) or die(mysql_error());
 $vurderinger = array();
 while($vurderinger[] = mysql_fetch_assoc($rc));
 array_pop($vurderinger);
 
+// finn alle artikler som er vurdert
+$sql = "SELECT k.artikkel_id, count(id) as ant FROM karakter k where k.karakter is not null group by k.artikkel_id ";
+$rc = mysql_query($sql, $apollon) or die(mysql_error());
+$karakterer = array();
+while($karakterer[] = mysql_fetch_assoc($rc));
+array_pop($karakterer);
 
 $count = 0;
 if (count($vurderinger)) {
@@ -39,9 +46,21 @@ if (count($published)) {
   }
 }
 
-/*
+
+// dersom denne eleven har $mine < 3 - da deler vi ut artikler
+if ($mine < 3) {
+  foreach($published as $p) {
+    if ($p["bruker_feide"] != $user and $p["ant"] < 4) {
+       // gi denne artikkelen til eleven
+       print_r($p);
+    }
+  }
+  
+}
+
+//*
 print '<pre>';
-print_r($artlist);
+print_r($published);
 print '</pre>';
-*/
+//*/
 ?>
