b6547621f7a10b2a2220918e141de6fdfdd6367a
Audun Hauge <audun.hauge@gmail.com>
2011-03-30 10:26:55 +0200

    fjerna fubar from henry

--- lastoppart.php
+++ lastoppart.php
@@ -8,9 +8,6 @@ $MM_donotCheckaccess = "true";
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
 {
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc('''''''''''''''') ? stripslashes($theValue) : $theValue;
-  }
 
   $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
 
