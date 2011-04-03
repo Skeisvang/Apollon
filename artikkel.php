6fd0c7eb43505fc798bb63e0ccdcdebe27f8c87d
Martin <martinsjursen93@gmail.com>
2011-04-03 15:52:54 +0200

    oppdatert

--- artikkel.php
+++ artikkel.php
@@ -1,6 +1,6 @@
-<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
 <?php
-require_once("Connections/apollon.php");
+require_once("access_check.php");
+
 mysql_select_db($database_apollon, $apollon);
 if (!isset($_GET['id']))
 	die("Du må spesifisere en artikkel!");
@@ -14,22 +14,27 @@ $artikkel = mysql_fetch_assoc($r);
 $qNavn = mysql_query("SELECT * FROM bruker WHERE feide = '" . $artikkel['bruker_feide'] ."'") or die(mysql_error());
 $aNavn = mysql_fetch_assoc($qNavn);
 $navn = $aNavn['fornavn'] . " " . $aNavn['etternavn'];
-
 ?>
-<html>
-	<head>
-    	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<link rel="stylesheet" href="page.css" type="text/css" />
-		<link rel="stylesheet" href="menu.css" type="text/css" />
-    </head>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+	<link rel="stylesheet" href="page.css" type="text/css" />
+	<link rel="stylesheet" href="menu.css" type="text/css" />
+</head>
+	
     <body>
-		<?php include 'menu.php' ?>
+        <?php include 'menu.php' ?>
 		
-		<div id="page_content">
-			<h2><?php echo $artikkel['overskrift']; ?></h2>
-			<small>Skrevet av <?php echo $navn; ?></small>
-			<p><?php echo $artikkel['artikkel']; ?></p>
-			<a href="brukerartikler.php?feide=<?php echo $artikkel['bruker_feide']; ?>">Tilbake</a>
-		</div>
+        <div id="page_content">
+            <h2><?php echo $artikkel['overskrift']; ?></h2>
+            <small>Skrevet av <?php echo $navn; ?></small>
+            <p><?php echo $artikkel['artikkel']; ?></p>
+            <a href="brukerartikler.php?feide=<?php echo $artikkel['bruker_feide']; ?>">Tilbake</a>
+        </div>
+        
+        <p></p>
     </body>
 </html>
